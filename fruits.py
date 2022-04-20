264ededbbe86e35d39294185f7d994b6b1caf67b
Santiago Rincon <chago591@hotmail.com>
2022-04-19 23:04:32 -0500

    deleted open

--- fruits.py
+++ fruits.py
@@ -3,9 +3,3 @@ with open("Fruits/vegetables.txt", "a+") as myfile:
     myfile.seek(0)
     content = myfile.read()
 print(content)
-
-with open("Fruits/vegetables.txt", "a+") as myfile:
-    myfile.write("Cucumber\nOnion\nTomato")
-    myfile.seek(0)
-    content = myfile.read()
-print(content)
